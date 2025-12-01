b0beb60a12cc3833194d07870b32ae5609a23ae3
Derrick <derrickmugisha169@gmail.com>
2025-12-01 19:00:00 +0200

    first commit

---
+++ src/main/java/com/igirerwanda/application_portal_backend/ApplicationPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.igirerwanda.application_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igirerwanda/application_portal_backend/ApplicationPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igirerwanda.application_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
